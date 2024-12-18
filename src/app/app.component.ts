4be61f07dc8df6a97237b6b6255708ddb8ba78f6
Antonella <antoh203@gmail.com>
2024-12-18 10:26:01 -0300

    components, core-directives and pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {COURSES} from '../db-data';
+import { Course } from './model/course';
 
 @Component({
     selector: 'app-root',
@@ -8,7 +9,15 @@ import {COURSES} from '../db-data';
     standalone: false
 })
 export class AppComponent {
+    courses = [...COURSES];
 
+    startDate = new Date(2000,0,1);
+    title = this.courses[0].description
+    course = this.courses[0];
 
-
+    onCoureSelected(course:Course){
+        console.log("card clicked" + course);
+    }
 }
+
+
---
+++ src/app/course-card/course-card.component.ts
@@ -0,0 +1,38 @@
+import { Component, Input, EventEmitter, Output } from '@angular/core';
+import { Course } from '../model/course';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'course-card',
+  imports: [CommonModule],
+  templateUrl: './course-card.component.html',
+  styleUrl: './course-card.component.css',
+  standalone: true
+})
+export class CourseCardComponent {
+  @Input({
+    required: true
+  }) course: Course;
+
+  @Input({
+    required: true
+  }) index: number
+
+  @Output()
+  courseSelected = new EventEmitter<Course>();
+
+  onCourseViewed() {
+    console.log("clicked")
+    this.courseSelected.emit(this.course);
+  }
+
+  cardClasses() {
+    if (this.course.category == "BEGINNER") {
+      return 'begginer';
+    }
+  }
+
+  cardStyles() {
+    return { 'text-decoration': 'underline' };
+  }
+}
